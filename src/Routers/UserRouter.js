a159cc5d4ef4b0446bbac1e41431d01a2eae554e
Chris Aragon <aragonchristopherray@gmail.com>
2020-09-02 10:55:55 -0400

    500 error in the testing files for users + user-songs

--- src/Routers/UserRouter.js
+++ src/Routers/UserRouter.js
@@ -9,8 +9,10 @@ userRouter
     .get((req, res, next) => {
         const knexInstance = req.app.get('db')
         UserService.getAllUsers(knexInstance)
+        //and there is an issue here for some reason :(
             .then(users => {
-                res.send(users)
+                console.log(users)
+                res.json(users)
             })
             .catch(next)
     })
--- src/Routers/UserSongsRouter.js
+++ src/Routers/UserSongsRouter.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const userSongsRouter = express.Router();
-const UserSongsServies = require('../Services/UserSongsServices');
+const UserSongsServices = require('../Services/UserSongsServices');
 //reminder: this one where users can log the songs they are working on!
 userSongsRouter
     .route('/')
     .get((req, res, next) => {
         const knexInstance = req.app.get('db');
         UserSongsServices.getAllUserSongs(knexInstance)
+        //apparently according to the testing file there is an issue here somewhere
             .then(userSongs => {
+                console.log(userSongs)
                 res.json(userSongs)
             })
-            .catch(next)
+            .catch(next);
     })
 
 module.exports = userSongsRouter
