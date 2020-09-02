baa1e86a7bbae60f9d5c0e991e3abc8e138a0469
Chris Aragon <aragonchristopherray@gmail.com>
2020-09-02 14:28:58 -0400

    wrote the post new user endpoint and it seems to work

--- src/Routers/UserRouter.js
+++ src/Routers/UserRouter.js
@@ -21,6 +21,26 @@ userRouter
         const knexInstance = req.app.get('db');
         const {username, password} = req.body;
         const newUser = {username, password};
+        console.log(newUser)
+        if(!username){
+            res.status(400).json('Username not included in request body; please enter a valid username')
+        } else if(username.length < 6 || username.length > 20){
+            res.status(400).json('Username must be between 6 and 20 characters')
+        } else if (!password){
+            res.status(400).json('Password not included in request body; please enter a valid password')
+        } else if(password.length < 8 || password.length > 36){
+            res.status(400).json('Password must be between 8 and 36 characters')
+        }
+        else{
+            UserService.postNewUser(knexInstance, newUser)
+                .then(user => {
+                    console.log(user)
+                    res.sendStatus(201)
+                })
+                .catch(next);
+        }
+        
+
     })
 
 module.exports = userRouter;
--- src/Services/UserService.js
+++ src/Services/UserService.js
@@ -1,6 +1,15 @@
 const UserService = {
     getAllUsers(knex){
         return knex.select().from('users');
+    },
+    postNewUser(knex, newUser){
+        return knex
+        .insert(newUser)
+        .into('users')
+        .returning('*')
+        .then(rows => {
+            return rows[0]
+        })
     }
 }
 
